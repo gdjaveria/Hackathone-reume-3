a8cb8c24d1c913fc82c7142103230bea8e4f7561
Javeriaiftikhar <126456366+gdjaveria@users.noreply.github.com>
2024-12-10 12:55:31 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+"use strict";
+document.addEventListener("DOMContentLoaded", () => {
+    const form = document.getElementById("resumeForm");
+    const resumeOutput = document.getElementById("resumeOutput");
+    form.addEventListener("submit", (event) => {
+        event.preventDefault();
+        // Data Collection..........
+        const data = {
+            personalInfo: {
+                name: document.getElementById('name').value,
+                email: document.getElementById('email').value,
+                phone: document.getElementById('phone').value,
+                address: document.getElementById('address').value,
+            },
+            education: document.getElementById('education').value,
+            experience: document.getElementById('experience').value,
+            skills: document.getElementById('skills').value,
+            description: document.getElementById('description').value,
+        };
+        generateResume(data, resumeOutput);
+    });
+});
+function generateResume(data, outputElement) {
+    outputElement.innerHTML =
+        ` <h2>Generated Resume</h2> 
+    <h3>Personal Information</h3>
+    <p><strong>Name:</strong> ${data.personalInfo.name}</p>
+     <p><strong>Email:</strong> ${data.personalInfo.email}</p>
+      <p><strong>Phone:</strong> ${data.personalInfo.phone}</p>
+       <p><strong>Address:</strong> ${data.personalInfo.address}</p>
+    
+    <h3>Education</h3>
+    <p>${data.education}</p>
+    <h3>Work Exprience</h3>
+    <p>${data.experience}</p>
+    <h3>Skills</h3>
+    <p>${data.skills}</p>
+    <h3>Description</h3>
+    <p>${data.description}</p> `;
+}
---
+++ script.ts
@@ -0,0 +1,61 @@
+interface ResumeData {
+  personalInfo:{
+    name:string,
+    email:string,
+    phone:string,
+    address:string,
+  }
+  education:string,
+  experience:string,
+  skills:string,
+  description:string,
+
+}
+
+document.addEventListener("DOMContentLoaded",() => {
+  const form =document.getElementById("resumeForm")as HTMLFormElement;
+  const resumeOutput =document.getElementById("resumeOutput")as HTMLDivElement;
+
+  form.addEventListener("submit",(event) => {
+    event.preventDefault()
+
+    // Data Collection..........
+
+const data: ResumeData = {
+  personalInfo:{
+    name:(document.getElementById('name')as HTMLInputElement).value,
+    email:(document.getElementById('email')as HTMLInputElement).value,
+    phone:(document.getElementById('phone')as HTMLInputElement).value,
+    address:(document.getElementById('address')as HTMLInputElement).value,
+
+  },
+  education:(document.getElementById('education')as HTMLInputElement).value,
+  experience:(document.getElementById('experience')as HTMLInputElement).value,
+  skills:(document.getElementById('skills')as HTMLInputElement).value,
+  description:(document.getElementById('description')as HTMLInputElement).value,
+ 
+};
+
+generateResume(data,resumeOutput);
+
+});
+});
+
+function generateResume(data: ResumeData, outputElement: HTMLDivElement):void{
+  outputElement.innerHTML =
+  ` <h2>Generated Resume</h2> 
+    <h3>Personal Information</h3>
+    <p><strong>Name:</strong> ${data.personalInfo.name}</p>
+     <p><strong>Email:</strong> ${data.personalInfo.email}</p>
+      <p><strong>Phone:</strong> ${data.personalInfo.phone}</p>
+       <p><strong>Address:</strong> ${data.personalInfo.address}</p>
+    
+    <h3>Education</h3>
+    <p>${data.education}</p>
+    <h3>Work Exprience</h3>
+    <p>${data.experience}</p>
+    <h3>Skills</h3>
+    <p>${data.skills}</p>
+    <h3>Description</h3>
+    <p>${data.description}</p> `
+}
